2142adf528e3eb47e3e338a6963ebb721e9a8545
Wayne Shelley <wael@bgs.ac.uk>
2022-03-12 21:55:39 +0000

    major refactor, improved and tested

--- Heater-Temp-Module.ino
+++ Heater-Temp-Module.ino
@@ -1,125 +1,165 @@
 #include <SoftwareSerialWithHalfDuplex.h>
+
 SoftwareSerialWithHalfDuplex sOne(2, 2); // NOTE TX & RX are set to same pin for half duplex operation
-int inhibswitch = 8;
-unsigned long lasttime;
-String heaterstate[] = {"Off","Starting","Pre-Heat","Failed Start - Retrying","Ignition - Now heating up","Running Normally","Stop Command Received","Stopping","Cooldown"};
-int heaterstatenum = 0;
-int heatererror = 0;
-String controlenable = "False";
-String inhibseenactive = "False";
-float currtemp = 0;
-float settemp = 0;
-float command = 0;
+
+int optionalEnableDisableSwitch = 8;
+unsigned long flasherLastTime;
+unsigned long rxLastTime;
+unsigned long writerLastTime;
+String heaterState[] = {"Off", "Starting", "Pre-Heat", "Failed Start - Retrying", "Ignition - Now heating up", "Running Normally", "Stop heaterCommand Received", "Stopping", "Cooldown"};
+int heaterStateNum = 0;
+int heaterError = 0;
+int controlEnable = 0;
+float currentTemperature = 0;
+float setTemperature = 0;
+float heaterCommand = 0;
 void setup()
 {
- pinMode(inhibswitch,INPUT_PULLUP);
-// initialize listening serial port
- // 25000 baud, Tx and Rx channels of Chinese heater comms interface:
- // Tx/Rx data to/from heater, special baud rate for Chinese heater controllers
- sOne.begin(25000);
- // initialise serial monitor on serial port 0
- Serial.begin(115200);
- // prepare for detecting a long delay
- lasttime = millis();
-
+  pinMode(optionalEnableDisableSwitch, INPUT_PULLUP);
+  // initialize listening serial port
+  // 25000 baud, Tx and Rx channels of Chinese heater comms interface:
+  // Tx/Rx data to/from heater, special baud rate for Chinese heater controllers
+  sOne.begin(25000);
+  // initialise serial monitor on serial port 0 - use 115200
+  // lower baud rates for debugging can slow down the diesel heater tx/rx
+  Serial.begin(115200);
+  // prepare for detecting a long delay
+  flasherLastTime = millis();
+  rxLastTime = millis();
+  writerLastTime = millis();
+    // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);      
 }
 
 void loop()
-{
- static byte Data[48];
- static bool RxActive = false;
- static int count = 0;
+{ 
+  static byte Data[48];
+  static bool RxActive = false;
+  static int count = 0;
+  // Default heartbeat LED flash length milliseconds
+  static long flashLength = 800;
 
+  unsigned long flasherTimeNow = millis();
+  unsigned long flasherDiff = flasherTimeNow - flasherLastTime;
 
- 
- // read from serial on D2
- if (sOne.available()) {
-
- // calc elapsed time since last rx’d byte to detect start of frame sequence
- unsigned long timenow = millis();
- unsigned long diff = timenow - lasttime;
- lasttime = timenow;
-
- if(diff > 100) { // this indicates the start of a new frame sequence
- RxActive = true;
- }
- int inByte = sOne.read(); // read hex byte
- if(RxActive) {
- Data[count++] = inByte;
- if(count == 48) {
- RxActive = false;
- }
- }
- }
-
- if(count == 48) { // filled both frames – dump
- count = 0;
-command = int(Data[2]);
-currtemp = Data[3];
-settemp = Data[4];
-heaterstatenum = int(Data[26]);
-heatererror = int(Data[41]);
-
-if (digitalRead(inhibswitch) == HIGH) {
-  inhibseenactive = "True";
-}
-
-if (digitalRead(inhibswitch) == LOW && (inhibseenactive == "True")) {
-  inhibseenactive = "False";
-  Serial.println("Inhibit Switch Toggled disable-enable - Enabling Auto");
-  controlenable = "True";
-}
-
- Serial.println();
- Serial.print("Command ");
- Serial.println(int(command));
- Serial.println(heaterstate[heaterstatenum]);
- Serial.print("Error Code ");
- Serial.println(heatererror);
- Serial.print("Current Temp ");
- Serial.println(int(currtemp));
- Serial.print("Set Temp ");
- Serial.println(int(settemp));
- Serial.print("System Enabled : ");
- Serial.println(controlenable);
-
-if (int(command) == 160) {
-  Serial.println("Start command seen from controller - Enabling Auto");
-  controlenable = "True";
-}
-if (int(command) == 5) {
-  Serial.println("Stop command seen from controller - Disabling Auto");
-  controlenable = "False";
-}
-
-
-
-  // Standard Range     Temp +2 to Temp -1 (3 degrees)
-
-if (controlenable == "True") {
-if (int(settemp) > int(currtemp) && (digitalRead(inhibswitch) == LOW) && (heatererror <= 1))  {
-  Serial.println("Temperature Below Lower Limit");
-  if (heaterstatenum == 0) {
-uint8_t data1[24] = {0x78,0x16,0xa0,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x32,0x08,0x23,0x05,0x00,0x01,0x2C,0x0D,0xAC,0x8D,0x82};
-delay(50);
-sOne.write(data1, 24);
+  // LED Heartbeat
+  // flash speed changes depending
+  // 1) standard heart beat when nothing is active
+  // 2) fast heart beat when heater has been triggered to turn on
+  // 3) slow heart beat when heater has been triggered to turn off
+  // These can be used for testing if you do not want to transmit changes to the heater.
+  if (controlEnable == 0) {
+    flashLength = 800;
   }
-}
-
- if (int(settemp) < (int(currtemp) - 1) && (digitalRead(inhibswitch) == LOW)) {
-  Serial.println("Temperature Above Upper Limit");
-  if (heaterstatenum == 5) {
-uint8_t data1[24] = {0x78,0x16,0x05,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x32,0x08,0x23,0x05,0x00,0x01,0x2C,0x0D,0xAC,0x61,0xD6};
-delay(50);
-sOne.write(data1, 24);
+  if (flasherDiff > flashLength) {
+    digitalWrite(LED_BUILTIN, HIGH);
   }
-}
-}
+  if (flasherDiff > flashLength * 2) {
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("Heartbeat");
+    flasherLastTime = flasherTimeNow;
+  }
 
+  // read from serial on D2
+  if (sOne.available()) {
+    // calc elapsed time since last rx’d byte to detect start of frame sequence
+    unsigned long rxTimeNow = millis();
+    unsigned long rxDiff = rxTimeNow - rxLastTime;
+    rxLastTime = rxTimeNow;
 
+    if (rxDiff > 100) { // this indicates the start of a new frame sequence
+      RxActive = true;
+    }
+    int inByte = sOne.read(); // read hex byte
+    if (RxActive) {
+      Data[count++] = inByte;
+      if (count == 48) {
+        RxActive = false;
+      }
+    }
+  }
 
+  if (count == 48) { // filled both frames – dump
+    unsigned long writerTimeNow = millis();
+    unsigned long writerDiff = writerTimeNow - writerLastTime;
 
- 
+    count = 0;
+    heaterCommand = int(Data[2]);
+    currentTemperature = Data[3];
+    setTemperature = Data[4];
+    heaterStateNum = int(Data[26]);
+    heaterError = int(Data[41]);
 
- }
+    // Data[29] looks like it contains voltage 121 = 12.1v
+    // Voltage dips significantly during glowplug use
+    // So low voltage detector would need to accomadate this
+    // Or thicker wires to the heater maybe required. 
+
+    // For debugging
+    //for (int i = 0; i <= 47; i++) {
+    //  Serial.print("Item ");
+    //  Serial.print(i);
+    //  Serial.print(": ");
+    //  Serial.println(int(Data[i]));
+    //}
+    Serial.println();
+    Serial.println("--- Heater Status ---");
+    Serial.print("Command          ");
+    Serial.println(int(heaterCommand));
+    Serial.print("Heater Status    ");
+    if (heaterStateNum >= 0 && heaterStateNum <= 8)
+    {
+      Serial.println(heaterState[heaterStateNum]);
+    }
+    Serial.print("Error Code       ");
+    Serial.println(heaterError);
+    Serial.print("Current Temp     ");
+    Serial.println(int(currentTemperature));
+    Serial.print("Set Temp         ");
+    Serial.println(int(setTemperature));
+    Serial.print("Heater State #   ");
+    Serial.println(heaterStateNum);
+    Serial.println();
+    Serial.print("System Enabled : ");
+    Serial.println(controlEnable);
+
+    if (int(heaterCommand) == 160) {
+      //Serial.println("Start command seen from controller - Enabling Auto");
+      controlEnable = 1;
+    }
+    if (int(heaterCommand) == 5) {
+      //Serial.println("Stop command seen from controller - Disabling Auto");
+      controlEnable = 0;
+    }
+
+    // Make sure variables are being populated by the heater
+    // Sometimes the current temp takes a while to establish
+    // Use writerDiff to wait 30 seconds between issuing commands
+    // Optional Switch is in correct position
+    if (controlEnable == 1 && int(currentTemperature) > 0 && int(setTemperature) > 0 && writerDiff > 30000 && digitalRead(optionalEnableDisableSwitch) == HIGH)
+    {
+      // Inside this block only gets run every 30 seconds
+      writerLastTime = writerTimeNow;
+      // Start heater if set temp is 2 degree higher than current temp by more than 1 degree and heater state is "[0] stopped"
+      if (int(setTemperature) >= (int(currentTemperature) + 2) && heaterError <= 1 && heaterStateNum == 0)  {
+        Serial.println("*** Temp Below Set Limit - Starting Heater ***");
+        uint8_t data1[24] = {0x78, 0x16, 0xa0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x32, 0x08, 0x23, 0x05, 0x00, 0x01, 0x2C, 0x0D, 0xAC, 0x8D, 0x82};
+        delay(50);
+        sOne.write(data1, 24);
+        // Now use 0.1 Second LED Heart Beat
+        flashLength = 100;
+      }
+
+      // shutdown if current temp is 3 degree warmer than set temp
+      if (int(setTemperature) <= (int(currentTemperature) - 3) && heaterStateNum == 5) {
+        Serial.println("*** Temperature Above Upper Limit - Stopping Heater ***");
+        uint8_t data1[24] = {0x78, 0x16, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x32, 0x08, 0x23, 0x05, 0x00, 0x01, 0x2C, 0x0D, 0xAC, 0x61, 0xD6};
+        delay(50);
+        sOne.write(data1, 24);
+        // Now use 3 Second LED Heart Beat
+        flashLength = 3000;
+      }
+    }
+
+  }
 } // loop
--- README.md
+++ README.md
@@ -1,15 +1,47 @@
-# Chinese-Heater-Temperature-Addon
-Addon for chinese diesel heaters to enable better (or different) temperature control
+# Chinese Diesel Heater - Advanaced Temperature Controller
+A simple Arduino inline extension to Chinese Diesel Heaters to help maintain a stable temperature by gracefully shutting down the heater if it gets too hot.
 
-This addon is written with the arduino nano as the target but should work with most 5v atmega chips.
+Keeping the existing controller/display, it does this by turning off the heater if the observed tempertaure goes beyond that set on the thermostat control. It will also automatically restart the heater if the temperature dips too cold. A temperature range is set to avoid excess power cyling.
 
-it connects directly to the data line between the heater and existing controller using D2 for data and a gnd connection to the heaters negative
-(in practice you can also pick up power from the same point on the loom as the existing controller is powered by 5v from the heater)
+Originally written by @Jess-- and recently refactored, improved and tested.
+
+Use at your own risk...
+## The Problem
+
+Ideally the standard heater behavour will maintain its own temperature, by reducing fuel and burn speed. Once the heater reaches the thermostat temperature it starts to slow down. However, the heater does not fully turn off. So if the heater is used in a small space, the minimum heat input can often be greater than that being lost. Then it gets hotter and hotter!
+## Hardware
+
+Keep the standard controller but splice into the 5V, GND and Blue control/data wire near the exising control/display unit.
+
+The 5V and GND connect to the Arduino VCC and GND pins. The blue data wire connects to the D2 pin. (check input voltage to the controller is definitely 5V - incase it is a different model)
+
+Confirmed working with the Arduino Nano 5V 16Mhz, but should work with most 5v ATMega chips.
+
+I have only tested it with the model I have but I suspect it will work with a range of heaters. The world famous "Afterburner" diesel heater controller by Ray Jones suggest this compatability list https://gitlab.com/mrjones.id.au/bluetoothheater/-/wikis/home#compatibility But thats only a guess, so use at your own risk!
+
+You can add an optional switch on to D8 Pin - shorting this pin to GND turns disables the new functionaility.
+## Behaviour 
 
 Once connected the addon will remain dormant until a start command is sent from the standard controller
 once it has seen the start command the module monitors temperature (desired temperature and actual temperature) data from the controller to the heater.
-If the actual temperature rises 2 degrees above the desired temperature and the heater has reached its normal running state the module will send a stop command to the heater, the command sent triggers the normal shutdown on the heater as if you had pressed the off button on the controller so all of the normal cooldown procedures are followed.
 
-If the heater has been shut down by the module and the temperature falls 1 degree below the desired temperature the module will issue a start command to the heater, again this start command triggers the same startup as you pressing the On button so all normal startup procedures are followed by the heater.
+If the actual temperature rises 3 degrees above the desired temperature and the heater has reached its normal running state the module will send a stop command to the heater, the command sent triggers the normal shutdown on the heater as if you had pressed the off button on the controller so all of the normal cooldown procedures are followed.
 
-The reason for 2 degrees above being chosen as the cutoff is to allow the heater to try and maintain temperature on its own by going to it's low setting (which it does at 1 degree above) and only shut it off if the amount of heat being produced on low is still raising the temperature
+If the heater has been shut down by the module and the temperature falls 2 degrees below the desired temperature the module will issue a start command to the heater, again this start command triggers the same startup as you pressing the On button so all normal startup procedures are followed by the heater.
+
+The reason for 3 degrees above being chosen as the cutoff is to allow the heater to try and maintain temperature on its own by going to it's low setting (which it does at 3 degree above) and only shut it off if the amount of heat being produced on low is still raising the temperature.
+
+## Todo
+
+- Further testing in the real-world.
+- Detecting the voltage and shutting it down if it drops below a certain voltage.
+## Change Log
+
+Changes since forked:
+- Refactored
+- Added 30 seconds delay before sending on/off commands.
+- Added extra temperature buffer to avoid cycling the heater excessively
+- Fixed potential array out of bounds issue
+- Commented out debug information for extra stability
+- Check Temperatures are reading correctly before starting the heater
+- Added LED heartbeat (normal speed for no changes, slow for low temp - turned on, fast for high temp - turned off)
