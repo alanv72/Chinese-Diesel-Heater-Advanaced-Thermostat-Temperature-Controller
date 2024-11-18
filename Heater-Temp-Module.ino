9931e08e9e9dbaebb6dce1df43ffe0f309b09ac5
Wayne Shelley <wael@bgs.ac.uk>
2024-11-18 22:20:57 +0000

    Fix negative temperature

--- Heater-Temp-Module.ino
+++ Heater-Temp-Module.ino
@@ -13,7 +13,9 @@ int heaterError = 0;
 // Default enable thermostat mode at start up
 int controlEnable = 1;
 
-float currentTemperature = 0;
+// set the currentTemperature default to something outside the usual range, this will help detect correctly reading the values.
+float currentTemperature = -200;
+
 float setTemperature = 0;
 float heaterCommand = 0;
 void setup()
@@ -88,10 +90,20 @@ void loop()
 
     count = 0;
     heaterCommand = int(Data[2]);
-    currentTemperature = Data[3];
-    setTemperature = Data[4];
+    currentTemperature = int(Data[3]);
+    setTemperature = int(Data[4]);
     heaterStateNum = int(Data[26]);
     heaterError = int(Data[41]);
+
+    // Adjust temperature integer for negative numbers (-1 = 255, -2 = 254)
+    // The basis of this code will use operating range -100'c to 155'c, which should be way outside the operating range of the heater
+    if (currentTemperature > 155 && currentTemperature <= 255) {
+      // currentTemperature will now give an accurate negative value
+      currentTemperature = currentTemperature - 256;
+      //  0 = 0
+      // -1 = 255, so 255-256=-1
+      // -2 = 254, so 254-256=-2
+    }
 
     // Data[29] looks like it contains voltage 121 = 12.1v
     // Voltage dips significantly during glowplug use
@@ -99,32 +111,32 @@ void loop()
     // Or thicker wires to the heater maybe required. 
 
     // For debugging
-    //for (int i = 0; i <= 47; i++) {
-    //  Serial.print("Item ");
-    //  Serial.print(i);
-    //  Serial.print(": ");
-    //  Serial.println(int(Data[i]));
-    //}
-    //Serial.println();
-    //Serial.println("--- Heater Status ---");
-    //Serial.print("Command          ");
-    //Serial.println(int(heaterCommand));
-    //Serial.print("Heater Status    ");
-    //if (heaterStateNum >= 0 && heaterStateNum <= 8)
-    //{
-    //  Serial.println(heaterState[heaterStateNum]);
-    //}
-    //Serial.print("Error Code       ");
-    //Serial.println(heaterError);
-    //Serial.print("Current Temp     ");
-    //Serial.println(int(currentTemperature));
-    //Serial.print("Set Temp         ");
-    //Serial.println(int(setTemperature));
-    //Serial.print("Heater State #   ");
-    //Serial.println(heaterStateNum);
-    //Serial.println();
-    //Serial.print("System Enabled : ");
-    //Serial.println(controlEnable);
+//    for (int i = 0; i <= 47; i++) {
+//      Serial.print("Item ");
+//      Serial.print(i);
+//      Serial.print(": ");
+//      Serial.println(int(Data[i]));
+//    }
+//    Serial.println();
+//    Serial.println("--- Heater Status ---");
+//    Serial.print("Command          ");
+//    Serial.println(int(heaterCommand));
+//    Serial.print("Heater Status    ");
+//    if (heaterStateNum >= 0 && heaterStateNum <= 8)
+//    {
+//      Serial.println(heaterState[heaterStateNum]);
+//    }
+//    Serial.print("Error Code       ");
+//    Serial.println(heaterError);
+//    Serial.print("Current Temp     ");
+//    Serial.println(currentTemperature);
+//    Serial.print("Set Temp         ");
+//    Serial.println(setTemperature);
+//    Serial.print("Heater State #   ");
+//    Serial.println(heaterStateNum);
+//    Serial.println();
+//    Serial.print("System Enabled : ");
+//    Serial.println(controlEnable);
 
     // On button pressed on unit or remote.
     if (int(heaterCommand) == 160) {
@@ -142,12 +154,12 @@ void loop()
     // Sometimes the current temp takes a while to establish
     // Use writerDiff to wait 30 seconds between issuing commands
     // Optional Switch is in correct position
-    if (controlEnable == 1 && int(currentTemperature) > 0 && int(setTemperature) > 0 && writerDiff > 30000 && digitalRead(optionalEnableDisableSwitch) == HIGH)
+    if (controlEnable == 1 && currentTemperature > -100 && setTemperature > 0 && writerDiff > 30000 && digitalRead(optionalEnableDisableSwitch) == HIGH)
     {
       // Inside this block only gets run every 30 seconds
       writerLastTime = writerTimeNow;
       // Start heater if set temp is 2 degree higher than current temp by more than 1 degree and heater state is "[0] stopped"
-      if (int(setTemperature) >= (int(currentTemperature) + 2) && heaterError <= 1 && heaterStateNum == 0)  {
+      if (setTemperature >= (currentTemperature + 2) && heaterError <= 1 && heaterStateNum == 0)  {
         //Serial.println("*** Temp Below Set Limit - Starting Heater ***");
         uint8_t data1[24] = {0x78, 0x16, 0xa0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x32, 0x08, 0x23, 0x05, 0x00, 0x01, 0x2C, 0x0D, 0xAC, 0x8D, 0x82};
         delay(50);
@@ -157,7 +169,7 @@ void loop()
       }
 
       // shutdown if current temp is 2 degree warmer than set temp
-      if (int(setTemperature) <= (int(currentTemperature) - 2) && heaterStateNum == 5) {
+      if (setTemperature <= (currentTemperature - 2) && heaterStateNum == 5) {
         //Serial.println("*** Temperature Above Upper Limit - Stopping Heater ***");
         uint8_t data1[24] = {0x78, 0x16, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x32, 0x08, 0x23, 0x05, 0x00, 0x01, 0x2C, 0x0D, 0xAC, 0x61, 0xD6};
         delay(50);
